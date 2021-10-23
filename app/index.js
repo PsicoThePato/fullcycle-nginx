6907523cf0b9adb7d17e4ad45a435d5ae1874dcb
PsicoThePato <jmomcontato@gmail.com>
2021-10-23 15:17:59 -0300

    :beers: :construction: I have absolutely no idea what im doing.js

--- app/index.js
+++ app/index.js
@@ -2,9 +2,39 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_ROOT_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE people (id int not null auto_increment, name VARCHAR(255), primary key(id))";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+});
+
+
+var faker = require('faker')
+
+
 app.get('/', (req,res) =>
 {
-    res.send('<h1> Full Cycle</h1>')
+    var randomName = faker.name.findName();
+    insertQuery = `INSERT INTO people(name) values(${randomName})`
+    con.query(sql)
+    con.end()
+    res.send('<h1>Pai Careca</h1>')
+    const getpeoplesql = 'SELECT name from people'
+
 })
 
 app.listen(port, ()=> 
